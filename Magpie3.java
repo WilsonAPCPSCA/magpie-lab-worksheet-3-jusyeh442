443ca5a04e0eaec699e03f2dceaeb10bc7476d24
Justin Yeh <32201151+jusyeh442@users.noreply.github.com>
2018-01-26 08:31:20 -0800

    Add files via upload

--- Magpie3.java
+++ Magpie3.java
@@ -8,15 +8,13 @@
  * @author Laurie White
  * @version April 2012
  */
-public class Magpie3
-{
+public class Magpie3 {
 	/**
 	 * Get a default greeting
 	 * 
 	 * @return a greeting
 	 */
-	public String getGreeting()
-	{
+	public String getGreeting() {
 		return "Hello, let's talk.";
 	}
 
@@ -27,28 +25,58 @@ public class Magpie3
 	 *            the user statement
 	 * @return a response based on the rules given
 	 */
-	public String getResponse(String statement)
-	{
+	public String getResponse(String statement) {
 		String response = "";
-		if (statement.length() == 0)
-		{
+		if (statement.length() == 0) {
 			response = "Say something, please.";
-		}
-		else if (findKeyword(statement, "no") >= 0)
-		{
+
+		} else if (findKeyword(statement, "no") >= 0) {
 			response = "Why so negative?";
-		}
-		else if (findKeyword(statement, "mother") >= 0
+
+		} else if (findKeyword(statement, "mother") >= 0
 				|| findKeyword(statement, "father") >= 0
 				|| findKeyword(statement, "sister") >= 0
 				|| findKeyword(statement, "brother") >= 0)
 		{
 			response = "Tell me more about your family.";
-		}
-		else
+
+		} else if (statement.indexOf("cat") >= 0 || statement.indexOf("dog") >= 0 ) {
+			response = "Tell me more about your pets.";
+
+		} else if (findKeyword(statement, "Bushyeager") >= 0 
+				|| findKeyword(statement, "Trimborn") >= 0
+				|| findKeyword(statement, "Rogan") >= 0
+				|| findKeyword(statement, "Arteaga") >= 0
+				|| findKeyword(statement, "Robles") >= 0
+				|| findKeyword(statement, "Young") >= 0)
 		{
+			if (findKeyword(statement, "Mr.") >= 0) {
+				response = "He sounds like a swell teacher.";
+			} else if (findKeyword(statement, "Mrs.") >= 0 || statement.indexOf("Ms.") >= 0) {
+				response = "She sounds like a swell teacher.";
+			} else {
+				response = "Your teacher sounds like a swell teacher.";
+			}
+
+		} else if (statement.trim().length() == 0) {	
+			response = "Say something, please.";
+
+		} else if (findKeyword(statement, "pizza") >= 0) {
+			response = "Hmmm, now I'm craving Italian.";
+
+		} else if (findKeyword(statement, "banana") >= 0) {
+			response = "Monkey!";
+
+		} else if (findKeyword(statement, "school") >= 0) {
+			response = "Try to keep up the good grades!";
+
+		} else if (findKeyword(statement, "hate") >= 0) {
+			response = "Hate is such a strong word.";
+
+		} else {
 			response = getRandomResponse();
 		}
+		
 		return response;
 	}
 
@@ -68,9 +96,8 @@ public class Magpie3
 	 * @return the index of the first occurrence of goal in
 	 *         statement or -1 if it's not found
 	 */
-	private int findKeyword(String statement, String goal,
-			int startPos)
-	{
+	private int findKeyword(String statement, String goal, int startPos) {
+
 		String phrase = statement.trim().toLowerCase();
 		goal = goal.toLowerCase();
 
@@ -80,40 +107,31 @@ public class Magpie3
 
 		// Refinement--make sure the goal isn't part of a
 		// word
-		while (psn >= 0)
-		{
+		while (psn >= 0) {
 			// Find the string of length 1 before and after
 			// the word
 			String before = " ", after = " ";
-			if (psn > 0)
-			{
+			if (psn > 0) {
 				before = phrase.substring(psn - 1, psn);
 			}
-			if (psn + goal.length() < phrase.length())
-			{
-				after = phrase.substring(
-						psn + goal.length(),
-						psn + goal.length() + 1);
-			}
 
+			if (psn + goal.length() < phrase.length()) {
+				after = phrase.substring(psn + goal.length(), psn + goal.length() + 1);
+			}
 			// If before and after aren't letters, we've
 			// found the word
-			if (((before.compareTo("a") < 0) || (before
-					.compareTo("z") > 0)) // before is not a
-											// letter
-					&& ((after.compareTo("a") < 0) || (after
-							.compareTo("z") > 0)))
+
+			if (((before.compareTo("a") < 0) || (before.compareTo("z") > 0)) // before is not a letter
+					&& ((after.compareTo("a") < 0) || (after.compareTo("z") > 0)))
 			{
 				return psn;
 			}
-
 			// The last position didn't work, so let's find
 			// the next, if there is one.
+
 			psn = phrase.indexOf(goal, psn + 1);
-
 		}
-
-		return -1;
+		return - 1;
 	}
 
 	/**
@@ -130,8 +148,7 @@ public class Magpie3
 	 * @return the index of the first occurrence of goal in
 	 *         statement or -1 if it's not found
 	 */
-	private int findKeyword(String statement, String goal)
-	{
+	private int findKeyword(String statement, String goal) {
 		return findKeyword(statement, goal, 0);
 	}
 
@@ -140,30 +157,28 @@ public class Magpie3
 	 * 
 	 * @return a non-committal string
 	 */
-	private String getRandomResponse()
-	{
-		final int NUMBER_OF_RESPONSES = 4;
+	private String getRandomResponse() {
+
+		final int NUMBER_OF_RESPONSES = 7;
 		double r = Math.random();
 		int whichResponse = (int) (r * NUMBER_OF_RESPONSES);
 		String response = "";
 
-		if (whichResponse == 0)
-		{
+		if (whichResponse == 0) {
 			response = "Interesting, tell me more.";
-		}
-		else if (whichResponse == 1)
-		{
+		} else if (whichResponse == 1) {
 			response = "Hmmm.";
-		}
-		else if (whichResponse == 2)
-		{
+		} else if (whichResponse == 2) {
 			response = "Do you really think so?";
-		}
-		else if (whichResponse == 3)
-		{
+		} else if (whichResponse == 3) {
 			response = "You don't say.";
+		} else if (whichResponse == 4) {
+			response = "Can you rephrase that?";
+		} else if (whichResponse == 5) {
+			response = "I have no response.";
+		} else if (whichResponse == 6) {
+			response = "Sorry, I don't understand.";
 		}
-
 		return response;
 	}
 
